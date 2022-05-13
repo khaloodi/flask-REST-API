3cd7f3e1714ce7a7257c34178dc52325c46a75f7
Khaled Adad <khaledadad@lewisu.edu>
2022-05-13 13:27:01 -0500

    startup a server

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask 
+
+DEBUG = True
+HOST = '0.0.0.0'
+PORT = 8000
+
+app = Flask(__name__) #flask pays attention to current file
+@app.route('/')
+def index():
+    return 'hi'
+
+if __name__ == '__main__':
+    app.run(debug=DEBUG, host=HOST, port=PORT)
