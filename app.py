710da8393dfb0f81ad0a918cf1b102e8f030586a
Khaled Adad <khaledadad@lewisu.edu>
2022-05-13 13:38:49 -0500

    created sqlite db w/2 tables... course and review

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask 
 
+import models
+
 DEBUG = True
 HOST = '0.0.0.0'
 PORT = 8000
@@ -10,4 +12,5 @@ def index():
     return 'hi'
 
 if __name__ == '__main__':
+    models.initialize()
     app.run(debug=DEBUG, host=HOST, port=PORT)
