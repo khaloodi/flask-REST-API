8c55c55296c4c962d9bd4c0475eae8b587b55fe5
Khaled Adad <khaledadad@lewisu.edu>
2022-05-13 17:56:55 -0500

    use marshal_with decorator to serialize json reply

--- resources/courses.py
+++ resources/courses.py
@@ -1,8 +1,8 @@
 from flask import jsonify #turn things into json responses
-from flask import Blueprint
+from flask import Blueprint, url_for, abort
 
 from flask_restful import (Resource, Api, reqparse ,marshal,
-                        marshal_with, inputs, fields)
+                        marshal_with, inputs, fields, url_for)
 
 import models
 
@@ -13,6 +13,18 @@ course_fields = {
     'url': fields.String, #note, fields.Url is used to point to another record w/in the Api, this is why we use fields.String
     'reviews': fields.List(fields.String)
 }
+
+def add_reviews(course):
+        course.reviews = [url_for('resources.reviews.review', id=review.id) for reveiw in course.review_set]
+        return course
+
+def course_or_404(course_id):
+    try:
+        course = models.Course.get(models.Course.id == course_id)
+    except models.Course.DoesNotExist:
+        abort(404)
+    else:
+        return course
 
 class CourseList(Resource): #resource for a list of courses
     #add parser
@@ -39,7 +51,7 @@ class CourseList(Resource): #resource for a list of courses
         # to handle the following: TypeError: Object of type ModelSelect is not JSON serializable
         # flask uses mashal:  ... [marshal() for item in select statement]
         # provide marshal with record or records and the fields defined for that resource
-        courses = [marshal(course, course_fields) for course in models.Course.select()] #get all the courses
+        courses = [marshal(add_reviews(course), course_fields) for course in models.Course.select()] #get all the courses
         # return jsonify({'courses': [{'title': 'Python Basics'}]})
         return jsonify({'courses': courses})
 
@@ -51,8 +63,27 @@ class CourseList(Resource): #resource for a list of courses
 
 
 class Course(Resource):
-    def get(self,id): #include id b/c individual courses will always get an id
-        return jsonify({'title': 'Python Basics'})
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument(
+            'title',
+            required=True,
+            help='No course title provided',
+            location=['form', 'json']
+        )
+        self.reqparse.add_argument(
+            'url',
+            required=True,
+            help='No course URL provided',
+            location=['form', 'json'],
+            type=inputs.url
+        )
+        super().__init__()
+
+    @marshal_with(course_fields) #marshal_with decorator useful for when working with one specific thing
+    def get(self, id): #include id b/c individual courses will always get an id
+        # return jsonify({'title': 'Python Basics'})
+        return add_reviews(course_or_404(id))
 
     def put(self,id): #include id b/c individual courses will always get an id
         return jsonify({'title': 'Python Basics'})
@@ -71,6 +102,6 @@ api.add_resource(#resource to add
 
 api.add_resource(#resource to add
     Course,
-    '/api/v1/course/<int:id>',
+    '/api/v1/courses/<int:id>',
     endpoint='course' #you don't have to provide the endpoint name, flask_restful just lowers the name of the resource if not provided
 )
